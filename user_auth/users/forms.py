bcac53be52a37e4e763e9d4c477666a13c249c5b
Kyle <rrblack701@gmail.com>
2025-05-15 23:35:24 +0900

    v

--- user_auth/users/forms.py
+++ user_auth/users/forms.py
@@ -1,5 +1,20 @@
+from django import forms
+from .models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
 class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ('email',)
+    password = forms.CharField(label='password', widget=forms.PasswordInput)
+    name = forms.CharField(label= 'name', max_length=100)
+    email = forms.CharField(label= 'email')
+
+    class Meta:
+        model = User
+        fields = ('name', 'email', 'password')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])  # hash the password
+        if commit:
+            user.save()
+        return user
--- user_auth/users/urls.py
+++ user_auth/users/urls.py
@@ -1,4 +1,3 @@
-from .views import login_view
 from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from . import views
@@ -7,9 +6,9 @@ from . import views
 
 urlpatterns = [
     path('', views.root_redirect),
-    path('login/', views.login_view, name='login'),
+    path('login/', views.users_login, name='user_login'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('top_page/', views.top_page, name='top_page'),
+    path('top_page/', views.login_success, name='top_page'),
     path('sign_up/', views.sign_up, name='sign_up'),
     path('mail_check/' , views.mail_check, name='mail_check'),
 
